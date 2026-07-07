b22361e42a0ecd8eaaeee0b15dc4e90c73602c07
YBY0119 <bingyuy43@gmail.com>
2026-07-07 11:31:10 +0800

    Change1

--- 0707/EvenOdd.java
+++ 0707/EvenOdd.java
@@ -3,8 +3,8 @@ public class EvenOdd {
 public static void main(String[]args){
     Scanner sc = new Scanner(System.in);
     System.out.print("Your Number:");
-    int num = sc.nextInt();
-    if(num % 2 == 0){
+    int number = sc.nextInt();
+    if(number % 2 == 0){
         System.out.println("Even");
     } else {
         System.out.println("Odd");
